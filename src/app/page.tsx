82c9b24ab861f3303cdbab7c147b2eab67021487
Gabriel <ga.lima2205@gmail.com>
2024-12-17 15:44:03 -0300

    up project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,20 @@
 
 import { useState } from "react";
 
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+
+const schema = yup
+  .object({
+    cep: yup
+      .string()
+      .min(8, "no minimo 8 caracteres")
+      .max(9, "no maximo 9 caracteres")
+      .required(),
+  })
+  .required();
+
 interface ICep {
   cep: string;
   logradouro: string;
@@ -16,42 +30,54 @@ interface ICep {
 }
 
 export default function Home() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
   const [data, setData] = useState<ICep | null>();
   const [cep, setCep] = useState<string>();
 
-  const Data = async () => {
-    setData(null);
-
-    if (!cep || cep.length < 8 || cep.length > 9) {
-      alert("Digite um cep valido");
-      return;
+  const submit = async () => {
+    try {
+      setData(null);
+      const res = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+      const resjson: ICep = await res.json();
+      setData(resjson);
+    } catch {
+      setCep("");
     }
-    const res = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
-    const resjson: ICep = await res.json();
-    if (resjson.cep === undefined) {
-      alert("Cep não encontrado");
-      return;
-    }
-    setData(resjson);
   };
 
   return (
     <>
       <div className="flex flex-col w-screen h-screen justify-center items-center">
         <div className="flex w-[19rem] items-center flex-col">
-          <input
-            placeholder="Digite o cep"
-            className="w-full rounded text-black p-[5px]"
-            value={cep}
-            onChange={(e) => setCep(e.target.value)}
-            type="text"
-          />
-          <button
-            onClick={Data}
-            className="block my-[10px] p-[5px] w-full rounded bg-white text-[#355867]"
-          >
-            Busca
-          </button>
+          <form onSubmit={handleSubmit(submit)}>
+            <input
+              {...register("cep")}
+              placeholder="Digite o CEP"
+              className="w-full rounded text-black p-[5px]"
+              value={cep || ""}
+              onChange={(e) => setCep(e.target.value)}
+              type="text"
+            />
+            {errors.cep ? (
+              <p className="rounded-sm mt-1 p-[1px] font-semibold text-[14px] bg-[#F7D3D4] text-[#AE666E]">
+                {errors.cep?.message}
+              </p>
+            ) : null}
+
+            <button
+              onClick={submit}
+              className="block my-[10px] p-[5px] w-full rounded bg-white text-[#355867]"
+            >
+              Busca
+            </button>
+          </form>
           {data ? (
             <>
               <div className="flex py-[45px] w-full px-[15px] rounded-xl bg-white flex-col">
